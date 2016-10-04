08cd7f54d0849f60036c3eb3475505c38521b315
Cesar Rodas <rodascesar03@yahoo.com>
2016-10-03 23:39:42 -0500

    began contact service construction

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,29 @@
+angular.module('contactApp', ['contactApp.models.contacts'])
+
+.controller('mainController', ['$scope', 'ContactsService', function($scope, ContactsService){
+	$scope.contacts;
+
+	ContactsService.getContacts().then(function(snapshot){
+			console.log(snapshot.key, snapshot.val());
+			var people = [];
+			var person = [];
+			snapshot.forEach(function(childSnapshot) {
+      	var key = childSnapshot.key;
+      	var childData = childSnapshot.val();
+      	person.push(key);
+      	person.push(childData);
+        people.push(person);
+  		});
+  		$scope.$apply(function (){
+  			$scope.contacts = JSON.stringify(people);
+  		});
+		},
+		function (e) {
+			console.log("Could not retrieve Contacts", e)
+		});
+}]);
+
+/*
 angular.module('contactApp', ['firebase'])
 
 .controller('mainController', ['$scope', '$firebaseObject', function($scope, $firebaseObject){
@@ -9,4 +35,5 @@ angular.module('contactApp', ['firebase'])
 	// 	console.log(data);
 	// });
 }]);
+*/
 
