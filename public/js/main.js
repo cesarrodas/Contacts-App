209b202dc5ef5309bafdeed25195319ab1f44d52
Cesar Rodas <rodascesar03@yahoo.com>
2016-10-04 22:25:59 -0500

    Finished contacts service

--- public/js/main.js
+++ public/js/main.js
@@ -1,39 +1,50 @@
 angular.module('contactApp', ['contactApp.models.contacts'])
 
 .controller('mainController', ['$scope', 'ContactsService', function($scope, ContactsService){
+	$scope.keys;
 	$scope.contacts;
 
-	ContactsService.getContacts().then(function(snapshot){
-			console.log(snapshot.key, snapshot.val());
-			var people = [];
-			var person = [];
-			snapshot.forEach(function(childSnapshot) {
-      	var key = childSnapshot.key;
-      	var childData = childSnapshot.val();
-      	person.push(key);
-      	person.push(childData);
-        people.push(person);
-  		});
-  		$scope.$apply(function (){
-  			$scope.contacts = JSON.stringify(people);
-  		});
-		},
-		function (e) {
-			console.log("Could not retrieve Contacts", e)
+	$scope.addContact = function(){
+		var fakeContact = {
+			name: "Candace",
+			age: 20,
+			bio: "this is my line"
+		}
+		ContactsService.createContact(fakeContact);
+	};
+
+	$scope.changeContact = function() {
+		var updating = $scope.keys[0];
+		var fakeContact = {
+			name: "Philip Defranco",
+			age: 28,
+			bio: "was up"
+		}
+		ContactsService.updateContact(updating, fakeContact);
+	};
+
+	$scope.delContact = function(){
+		var deleting = $scope.keys[0];
+		ContactsService.removeContact(deleting);
+	};
+
+	$scope.getAllContacts = function() {
+		contactsRef.once("value", function(snapshot) {
+			$scope.$apply(function(){
+				$scope.keys = Object.keys(snapshot.val());
+				$scope.contacts = snapshot.val();
+			});
+		}, function (errorObject) {
+  		console.log("Failed to load Contacts " + errorObject.code);
 		});
+	}
+
+	contactsRef.on("value", function(snapshot) {
+		$scope.keys = Object.keys(snapshot.val());
+		$scope.contacts = snapshot.val();
+	}, function (errorObject) {
+  	console.log("The read failed: " + errorObject.code);
+	});
+
 }]);
-
-/*
-angular.module('contactApp', ['firebase'])
-
-.controller('mainController', ['$scope', '$firebaseObject', function($scope, $firebaseObject){
-
-	var ref = firebase.database().ref();
-	$scope.name = $firebaseObject(ref);
-	// $scope.people = $http.get('../json/people.json').success(function(data){
-	// 	console.log("GOT THAT DATA BRO");
-	// 	console.log(data);
-	// });
-}]);
-*/
 
