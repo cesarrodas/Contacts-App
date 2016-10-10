0042424aae759078a19ba5b2b5436465ea8f3b4b
Cesar Rodas <rodascesar03@yahoo.com>
2016-10-09 20:02:54 -0500

    Added toast messages

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-angular.module('contactApp', ['ui.router', 'angular-ladda', 'contacts', 'createContact','contactApp.models.contacts'])
+angular.module('contactApp', ['ui.router', 'angular-ladda','toaster', 'ngAnimate', 'contacts', 'createContact','contactApp.models.contacts'])
 
 .config(function ($stateProvider, $urlRouterProvider) {
 
